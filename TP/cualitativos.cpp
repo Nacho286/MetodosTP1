f0f3b502212467c1b3a6a816c62e1d373b47393c
Ignacio Roberto Rodriguez <irodriguez@ws24.labo7>
2017-04-10 20:14:23 -0300

    TEST

--- TP/cualitativos.cpp
+++ TP/cualitativos.cpp
@@ -10,18 +10,20 @@
 
 
 int main (int args, char* argsv[]) {
-	
+
 	int equipos = archivos::cantEquipos(argsv[1]);
+
 	double r[equipos];
 	double r2[equipos];
+
 	double total[equipos]; // Para caso WP
 	vector< vector<double> > matriz;
 
 	// Para EG, uso una matriz de n x (n + 1) (la ultima columna es el b)
 	matriz.resize(equipos, vector<double>(equipos + 1));
 	matriz = archivos::armar_matriz_eg(argsv[1]);
-	//archivos::armar_wp(argsv[1], r2, total);
-			
+	archivos::armar_wp(argsv[1], r2, total);
+	
 	SparseMatrix rala = SparseMatrix(matriz, equipos, equipos+1);
 	rala.eg(r);		
 
@@ -31,7 +33,15 @@ int main (int args, char* argsv[]) {
 	// Se respeta el orden de los equipos en el archivo de salida
 	for (int j = 0; j < equipos; j++)
 	 	salida << r[j] << "\n";
+	//salida.close();
+
+	for (int i = 0; i < equipos; i++)
+		r2[i] = r2[i] / total[i];
+
+	for (int j = 0; j < equipos; j++)
+	 	salida << r2[j] << "\n";
 	salida.close();
+
 
 	return 0;
 }
