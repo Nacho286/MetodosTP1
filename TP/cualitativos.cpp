0807ed47e886ef8dd70f4f78493d182f842002f8
Nicolas Mastropasqua <nmastropasqua@ws5.labo7>
2017-04-10 16:51:24 -0300

    Intento de archivo de test cualitativos Por alguna razon no parsea bien el tesNuestro1.in ! (seg fault)

---
+++ TP/cualitativos.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <cmath>
+#include "tiempo.h"
+#include "SparseMatrix.h"
+#include "archivos.h"
+
+
+int main (int args, char* argsv[]) {
+	
+	int equipos = archivos::cantEquipos(argsv[1]);
+	double r[equipos];
+	double r2[equipos];
+	double total[equipos]; // Para caso WP
+	vector< vector<double> > matriz;
+
+	// Para EG, uso una matriz de n x (n + 1) (la ultima columna es el b)
+	matriz.resize(equipos, vector<double>(equipos + 1));
+	matriz = archivos::armar_matriz_eg(argsv[1]);
+	//archivos::armar_wp(argsv[1], r2, total);
+			
+	SparseMatrix rala = SparseMatrix(matriz, equipos, equipos+1);
+	rala.eg(r);		
+
+	// Los arreglos se pasan como punteros, r contiene el resultado
+	ofstream salida;
+	salida.open(argsv[2]);
+	// Se respeta el orden de los equipos en el archivo de salida
+	for (int j = 0; j < equipos; j++)
+	 	salida << r[j] << "\n";
+	salida.close();
+
+	return 0;
+}
