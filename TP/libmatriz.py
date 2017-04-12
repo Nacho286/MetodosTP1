d9d8630bf631bf6131bc3ea770b34bcae6a3f9ff
klascolin <klascolin@gmail.com>
2017-04-12 01:18:12 -0300

    Había un error al generar las matrices de Colley. Arreglado

--- TP/libmatriz.py
+++ TP/libmatriz.py
@@ -7,13 +7,17 @@ def generar_Colley(n, m):
 	total = 0
 	for i in xrange(n):
 		n_i = 2
-		for j in xrange(i + 1, n):
-			n_ij = ran.randint(0, 3) 	# 3 partidos jugados entre equipos como maximo
-			colley[i][j] = (-1) * n_ij
-			colley[j][i] = colley[i][j]
-			n_i += n_ij
+		for j in xrange(n): 	# 3 partidos jugados entre equipos como maximo
+			if(j>i):
+				n_ij = ran.randint(0, 3)
+				colley[i][j] = (-1) * n_ij
+				colley[j][i] = colley[i][j]
+				n_i += n_ij
+			else:
+				n_i += abs(colley[i][j])
 		colley[i][i] = n_i
 		total += n_i - 2
+	print colley
 	generar_partidos(colley, n, m, total)
 
 def generar_partidos(matriz, n, m, partidos):
